8066fa7932fcf7ddae3d37f4c766e1f0e5ef0e67
Mauro Vieira <maurosilva.aj@gmail.com>
2015-08-29 00:16:51 -0300

    Menu de topo mobile e comentarios

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,9 @@
- $(document).ready(function(){
+
+/*
+**script menu mobile**
+*/
+$(document).ready(function(){
       $('.dropdown .btn_n').click(function(){
-        $('.dropdown ul').toggle();
-      });
-      
+        $('.dropdown ul').toggle('slow');
+      });      
     });
